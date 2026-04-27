052041bd34de5d2a5a24945be238bfb75c481b12
Maroshop <homworkhhh25@gmail.com>
2026-04-27 15:01:00 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCzOjPKrd8bQP3A58g0hNqjJAnNL3mQyhw",
+    authDomain: "arslan-b6498.firebaseapp.com",
+    databaseURL: "https://arslan-b6498-default-rtdb.firebaseio.com",
+    projectId: "arslan-b6498",
+    storageBucket: "arslan-b6498.firebasestorage.app",
+    messagingSenderId: "18679743346",
+    appId: "1:18679743346:web:8e6c8ae8d57b7a0b63d3eb",
+    measurementId: "G-TB1QDT50Y6"
+  },
+
+  firebaseRoot: "cashier_units_pro_arslan_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_arslan_885766842"
+  ]
+};
